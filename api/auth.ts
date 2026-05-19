67eceb0c7d280c661a965877f074575b5b99437a
kenza-azhar <agusarfien@madrasahebat.com>
2026-05-19 19:22:32 +0700

    Update auth.ts

--- api/auth.ts
+++ api/auth.ts
@@ -1,4 +1,4 @@
-import { sql } from './db';
+import { sql } from './db.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { VercelRequest, VercelResponse } from '@vercel/node';
