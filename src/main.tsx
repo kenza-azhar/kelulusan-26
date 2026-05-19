363c4c6a3f20ef8939e44d05ee5b841e0b7a591c
kenza-azhar <agusarfien@madrasahebat.com>
2026-05-19 18:03:20 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+Response.prototype.json = async function safeJson() {
+  const text = await this.text();
+  if (!text) {
+    return {};
+  }
+  try {
+    return JSON.parse(text);
+  } catch {
+    return {};
+  }
+};
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
